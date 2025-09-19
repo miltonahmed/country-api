d4c16a555f009ec95fb814222cb39dce1f9c4ecb
Milton Ahmed <miltonahmed08@yahoo.com>
2025-09-20 02:41:29 +0400

    Work finished

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,15 +1,20 @@
-import { use } from "react";
+import { use } from 'react';
+import Country from '../country/Country';
 
 const Countries = ({ countriesData }) => {
-  const countriesApi= use(countriesData)
-  const countries = countriesApi.countries
-  console.log(countries)
+  const countriesApi = use(countriesData);
+  const countries = countriesApi.countries;
+
   return (
     <div>
-    <h1>Countries : {countries.length}</h1>
-
+      <h1 className='text-4xl text-black font-bold text-center uppercase mb-3 '>In The Countries : {countries.length}</h1>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 text-xl sm:text-2xl text-black capitalize'>
+        {countries.map(country => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
     </div>
-  )
+  );
 };
 
-export default Countries
+export default Countries;
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,8 +1,18 @@
 
-const Country = () => {
+const Country = ({country}) => {
+  console.log(country.languages.languages);
   return (
-    <div>Country</div>
-  )
+    <div className="border border-amber-300 rounded-2xl shadow py-3 px-3 flex justify-center items-center flex-col space-y-2.5 capitalize text-center">
+      <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+      <p>{country.name.official}</p>
+      <p>{country.name.common}</p>
+      <span>{country.name.region}</span>
+      <h2>Population:{country.population.population}</h2>
+      <h3>{country.area.area > 30000 ? '(Big Country)' : '(Small Country}'}</h3>
+      <p> Region: {country.region.region}</p>
+      <p> Capital: {country.capital.capital}</p>
+    </div>
+  );
 }
 
 export default Country
